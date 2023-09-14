837baffeaa7a4ae138403032296f90f4e36215ac
Gergely Pasztor <gpasztor@3-G-11.42heilbronn.de>
2023-09-14 14:06:07 +0200

    Portal working properly

--- mandatory/src/portal.c
+++ mandatory/src/portal.c
@@ -6,24 +6,45 @@
 /*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 12:25:04 by gpasztor          #+#    #+#             */
-/*   Updated: 2023/09/12 19:23:10 by gpasztor         ###   ########.fr       */
+/*   Updated: 2023/09/14 14:02:34 by gpasztor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-void	teleport(t_cubdata *data, t_player *player, int x, int y)
+void	teleport(t_cubdata *data, t_int_vector ppos, char dir)
 {
+	if (dir == 'N' && data->worldmap[ppos.y - 1][ppos.x] == '0')
+	{
+		data->player->pos.x = ppos.x + 0.5;
+		data->player->pos.y = ppos.y - 0.5;
+	}
+	else if (dir == 'S' && data->worldmap[ppos.y + 1][ppos.x] == '0')
+	{
+		data->player->pos.x = ppos.x + 0.5;
+		data->player->pos.y = ppos.y + 1.5;
+	}
+	else if (dir == 'E' && data->worldmap[ppos.y][ppos.x + 1] == '0')
+	{
+		data->player->pos.x = ppos.x + 1.5;
+		data->player->pos.y = ppos.y + 0.5;
+	}
+	else if (dir == 'W' && data->worldmap[ppos.y][ppos.x - 1] == '0')
+	{
+		data->player->pos.x = ppos.x - 0.5;
+		data->player->pos.y = ppos.y + 0.5;
+	}
+}
+
+void	second_teleport_check(t_cubdata *data, int x, int y, char dir)
+{
+	t_player		*player;
+
+	player = data->player;
 	if (x == data->parser->ppos[0].x && y == data->parser->ppos[0].y)
-	{
-		player->pos.x = data->parser->ppos[1].x + 0.5;
-		player->pos.y = data->parser->ppos[1].y + 0.5;
-	}
+		teleport(data, data->parser->ppos[1], dir);
 	else if (x == data->parser->ppos[1].x && y == data->parser->ppos[1].y)
-	{
-		player->pos.x = data->parser->ppos[0].x + 0.5;
-		player->pos.y = data->parser->ppos[0].y + 0.5;
-	}
+		teleport(data, data->parser->ppos[0], dir);
 }
 
 void	teleport_check(t_cubdata *data, t_player *player, char dir)
@@ -31,11 +52,11 @@ void	teleport_check(t_cubdata *data, t_player *player, char dir)
 	if (dir == '0')
 		return ;
 	if (dir == 'N')
-		teleport(data, data->player, player->pos.x, player->pos.y - 1);
+		second_teleport_check(data, player->pos.x, player->pos.y - 1, dir);
 	else if (dir == 'S')
-		teleport(data, data->player, player->pos.x, player->pos.y + 1);
+		second_teleport_check(data, player->pos.x, player->pos.y + 1, dir);
 	else if (dir == 'E')
-		teleport(data, data->player, player->pos.x + 1, player->pos.y);
+		second_teleport_check(data, player->pos.x + 1, player->pos.y, dir);
 	else if (dir == 'W')
-		teleport(data, data->player, player->pos.x - 1, player->pos.y);
+		second_teleport_check(data, player->pos.x - 1, player->pos.y, dir);
 }
